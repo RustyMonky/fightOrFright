70727a351f9a81993a5472ecabe864eba0f369d0
Ian <ian.cantor@fandm.edu>
2018-10-03 21:10:48 -0400

    Creates bullets and bullet basic position updating logic

---
+++ actors/projectiles/bullet/bullet.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+onready var horizontalTexture = load("res://assets/sprites/projectiles/bullet/bulletHoriz.png")
+onready var sprite = $Sprite
+onready var verticalTexture = load("res://assets/sprites/projectiles/bullet/bulletVert.png")
+
+var direction
+
+const SPEED = 300
+
+func _ready():
+	self.direction = get_parent().direction
+
+	if self.direction.y == 0: # Horizontal
+		sprite.set_texture(horizontalTexture)
+		if self.direction.x == 1:
+			sprite.flip_h = true
+	elif self.direction.x == 0:
+		sprite.set_texture(verticalTexture)
+		if self.direction.y == 1:
+			sprite.flip_v = true
+	set_process(true)
+
+func _process(delta):
+	if self.direction == Vector2(0, 0):
+		self.direction = Vector2(-1, 0) # Unfortunately, required override for bullet spawn if player doesn't move
+	self.global_position += self.direction.normalized() * SPEED * delta
+
+	if self.global_position.x < 0 || self.global_position.x > OS.get_screen_size().x || self.global_position.y < 0 || self.global_position.y > OS.get_screen_size().y:
+		self.queue_free()
+	
--- actors/timmy/timmy.gd
+++ actors/timmy/timmy.gd
@@ -2,28 +2,27 @@ extends KinematicBody2D
 
 enum STATE { REST, MOVING }
 
-var animations
+onready var animations = $animations
 var currentState = STATE.REST
 var direction = Vector2(0, 0)
 const SPEED = 2
 
 func _ready():
-	animations = $animations
 	set_process(true)
 	set_process_input(true)
 
 func _process(delta):
 	if Input.is_action_pressed("ui_up"):
-		direction = Vector2(0, -1)
+		self.direction = Vector2(0, -1)
 		move_timmy()
 	elif Input.is_action_pressed("ui_down"):
-		direction = Vector2(0, 1)
+		self.direction = Vector2(0, 1)
 		move_timmy()
 	elif Input.is_action_pressed("ui_left"):
-		direction = Vector2(-1, 0)
+		self.direction = Vector2(-1, 0)
 		move_timmy()
 	elif Input.is_action_pressed("ui_right"):
-		direction = Vector2(1, 0)
+		self.direction = Vector2(1, 0)
 		move_timmy()
 
 	if currentState == STATE.REST:
@@ -32,9 +31,24 @@ func _process(delta):
 			animations.set_frame(0)
 
 func _input(event):
+	# Bullet functionality does not depend on MOVING or REST states
+	if event.is_action_pressed("ui_select"):
+		fire()
+
 	if currentState == STATE.MOVING:
 		if event.is_action_released("ui_up") || event.is_action_released("ui_down") || event.is_action_released("ui_left") || event.is_action_released("ui_right"):
 			currentState = STATE.REST
+
+func fire():
+	var bulletInstance = load("res://actors/projectiles/bullet/bullet.tscn").instance()
+	self.add_child(bulletInstance)
+
+	# Unfortunately, bullet position is hard set so that it exits the gun sprite
+	if (self.direction.x < 1):
+		bulletInstance.position = Vector2(-14, 11)
+	else:
+		bulletInstance.position = Vector2(14, 11)
+	bulletInstance.direction = self.direction
 
 func move_timmy():
 	currentState = STATE.MOVING
