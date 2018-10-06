b93696fc58f60da0c4f93097d59303b2739a5062
Ian <ian.cantor@fandm.edu>
2018-10-06 13:39:30 -0400

    Now spawns mobs on both sides of screen, increases bullet size

--- actors/enemies/skeleton/skeleton.gd
+++ actors/enemies/skeleton/skeleton.gd
@@ -1,12 +1,11 @@
 extends KinematicBody2D
 
-var direction = Vector2(1, 0)
+var direction
 var hp = 2
 
 const SPEED = 32
 
 func _ready():
-	print("added at " + String(self.position))
 	set_physics_process(true)
 
 func _physics_process(delta):
--- actors/enemies/spawner.gd
+++ actors/enemies/spawner.gd
@@ -9,7 +9,16 @@ func _on_spawnTimer_timeout():
 	randomize()
 	# Create a new enemy on every timeout
 	var enemy = load("res://actors/enemies/skeleton/skeleton.tscn").instance()
-	var spawnHeight = int(rand_range(32, int(OS.window_size.y))) #randi() % int(OS.window_size.y) # Temporary
-	enemy.position = Vector2(-32, spawnHeight)
+	var spawnHeight = int(rand_range(32, int(OS.window_size.y))) # Temporary
+
+	# Determine side of the screen
+	var sideId = randi() % 2
+	if sideId == 0:
+		enemy.position = Vector2(-32, spawnHeight)
+		enemy.direction = Vector2(1, 0)
+	else:
+		enemy.position = Vector2(int(OS.window_size.x + 32), spawnHeight)
+		enemy.direction = Vector2(-1, 0)
+		enemy.get_node("animations").flip_h = true
 	get_parent().add_child(enemy) # Assumes parent is level scene!
 	
