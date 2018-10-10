bd9b0e5eb536e25a478ac1729d74b8e8b146ab74
Ian <ian.cantor@fandm.edu>
2018-10-09 23:06:57 -0400

    W.i.P. skeleton attacks

--- actors/enemies/skeleton/skeleton.gd
+++ actors/enemies/skeleton/skeleton.gd
@@ -1,22 +1,33 @@
 extends KinematicBody2D
 
-enum STATE {ALIVE, FOLLOW, DEAD}
+enum STATE {ALIVE, ATTACK, FOLLOW, DEAD}
 
 onready var animations = $animations
 onready var collider = $collider
 onready var fadeTimer = $fadeTimer
 onready var fadeTween = $fadeTween
+onready var ray = $ray
 onready var timmy = get_parent().get_node("timmy")
 
+var attackAnimation = load("res://assets/spriteFrames/enemies/skeleton/skeletonAttack.tres")
 var currentState = STATE.ALIVE
 var direction
 var hp = 2
+var walkAnimation = load("res://assets/spriteFrames/enemies/skeleton/skeletonWalk.tres")
 
 const SPEED = 32
 
 func _ready():
 	animations.playing = true
 	set_physics_process(true)
+	set_process(true)
+
+func _process(delta):
+	if ray.is_colliding():
+		if ray.get_collider().is_in_group("timmy"):
+			currentState = STATE.ATTACK
+			animations.set_sprite_frames(attackAnimation)
+			animations.set_animation("skeletonAttack")
 
 func _physics_process(delta):
 	if currentState == STATE.ALIVE:
@@ -48,6 +59,10 @@ func take_damage():
 func _on_animations_animation_finished():
 	if currentState == STATE.DEAD:
 		fadeTimer.start()
+	elif currentState == STATE.ATTACK:
+		currentState = STATE.FOLLOW
+		animations.set_sprite_frames(walkAnimation)
+		animations.set_animation("skeletonMove")
 
 func _on_fadeTimer_timeout():
 	fadeTween.interpolate_property(self, "modulate", Color(1,1,1,1), Color(1,1,1,0), 1, Tween.TRANS_LINEAR, Tween.EASE_OUT)
