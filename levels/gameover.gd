41d8bb5eaa9c74842ea63bc7e37b8aff7ee7cdc8
Ian <ian.cantor@fandm.edu>
2018-10-17 22:33:37 -0400

    Adds logic to return to title from gameover

--- levels/gameover.gd
+++ levels/gameover.gd
@@ -7,6 +7,12 @@ func _ready():
 	mobsLabel.text = String(gameData.mobsKilled) + ' mobs slain'
 	timesLabel.text = formattedTime() + ' spent alive'
 
+	set_process_input(true)
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		sceneManager.goto_scene("res://levels/title.tscn")
+
 func formattedTime():
 	if gameData.secondsAlive < 10:
 		gameData.secondsAlive = '0' + String(gameData.secondsAlive)
